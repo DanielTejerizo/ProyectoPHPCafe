807ee6c2c0c92ff9105c4fd13b1e84c14047fb1d
DanielTejerizo <daniel.tejmor@educa.jcyl.es.com>
2023-11-11 15:51:15 +0100

    .

--- admin/Baja-proveedores.php
+++ admin/Baja-proveedores.php
@@ -1,23 +1,34 @@
 <?php
 include('../conexion.php'); // Asegúrate de ajustar la ruta correctamente
 
-// Verificar si se ha enviado el formulario
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// Verificar si se ha enviado el formulario y si la clave "id" está definida
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["id"])) {
     // Obtener el valor del ID del formulario
     $id = $_POST["id"];
 
     // Conectar a la base de datos
     $conexion = conectar();
-    // Preparar la consulta de eliminación
-    $secuencia = $conexion->prepare("DELETE FROM proveedores WHERE idProveedor = ?");
-    $secuencia->bind_param("i", $id);
 
-    // Ejecutar la consulta
-    if ($secuencia->execute()) {
-        echo "<script>mostrarAlerta();</script>";
-        echo "Proveedor eliminado exitosamente";
+    // Verificar si el proveedor existe antes de eliminarlo
+    $consulta_existencia = $conexion->prepare("SELECT idProveedor FROM proveedores WHERE idProveedor = ?");
+    $consulta_existencia->bind_param("i", $id);
+    $consulta_existencia->execute();
+    $consulta_existencia->store_result();
+
+    if ($consulta_existencia->num_rows > 0) {
+        // Preparar la consulta de eliminación
+        $secuencia = $conexion->prepare("DELETE FROM proveedores WHERE idProveedor = ?");
+        $secuencia->bind_param("i", $id);
+
+        // Ejecutar la consulta
+        if ($secuencia->execute()) {
+            echo "<script>mostrarAlerta();</script>";
+            echo "Proveedor eliminado exitosamente";
+        } else {
+            echo "Error al eliminar el proveedor: " . $secuencia->error;
+        }
     } else {
-        echo "Error al eliminar el proveedor: " . $secuencia->error;
+        echo "No existe un proveedor con ese ID.";
     }
 
     // Cerrar la conexión
@@ -31,6 +42,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Eliminación de Proveedores</title>
     <link rel="stylesheet" href="../css/Alta.css">
     <script>
+        function confirmarEliminacion() {
+            return confirm("¿Estás seguro de que deseas eliminar este proveedor?");
+        }
+
         function mostrarAlerta() {
             alert("Proveedor eliminado correctamente");
         }
@@ -41,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <h1>Eliminación de Proveedores</h1>
 
     <!-- Formulario para eliminar un proveedor por su ID -->
-    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>" onsubmit="return confirmarEliminacion();">
         <label for="id">ID del Proveedor a eliminar:</label>
         <input type="text" name="id" id="id" size="10" required><br><br>
         <button type="submit">Eliminar Proveedor</button>
