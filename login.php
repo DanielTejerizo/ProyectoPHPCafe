e65f2fb97e1a63d2366257c0b859155b86c22382
Daniel Tejerizo <daniel.tejmor@educa.jcyl.es>
2023-11-13 19:53:39 +0100

    .

--- login.php
+++ login.php
@@ -1,11 +1,14 @@
 <?php
 include('conexion.php');
 
+// Inicializar mensaje de error
+$mensajeError = "";
+
 // Verificar si se envió el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recoger datos del formulario
-    $nombreUsuario = $_POST["nombreUsuario"];
-    $contrasena = $_POST["contrasena"];
+    // Limpiar y recoger datos del formulario
+    $idUsuario = limpiarDatos($_POST["idUsuario"]);
+    $contrasena = limpiarDatos($_POST["contrasena"]);
 
     $conexion = conectar();
 
@@ -14,8 +17,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Consulta para obtener la contraseña almacenada
-    $consulta = $conexion->prepare("SELECT idUsuario, Contrasenia, Tipo FROM Usuarios WHERE NombreUsuario = ?");
-    $consulta->bind_param("s", $nombreUsuario);
+    $consulta = $conexion->prepare("SELECT idUsuario, Contrasenia, Tipo FROM Usuarios WHERE idUsuario = ?");
+    $consulta->bind_param("i", $idUsuario);
 
     if ($consulta->execute()) {
         $consulta->store_result();
@@ -31,6 +34,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $_SESSION['idUsuario'] = $idUsuario;
                 $_SESSION['tipoUsuario'] = $tipoUsuario;
 
+                // Regenerar el ID de sesión para evitar ataques de secuestro de sesión
+                session_regenerate_id(true);
+
                 // Redireccionar a la página después del inicio de sesión
                 header("Location: pagina_protegida.php");
                 exit();
@@ -38,13 +44,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $mensajeError = "La contraseña es incorrecta.";
             }
         } else {
-            $mensajeError = "El nombre de usuario no existe.";
+            $mensajeError = "ID de usuario no encontrado.";
         }
     } else {
-        $mensajeError = "Error al realizar la consulta: " . $conexion->error;
+        // Log de errores en lugar de mostrar al usuario
+        error_log("Error al realizar la consulta: " . $conexion->error);
+        $mensajeError = "Error en el inicio de sesión. Por favor, inténtelo de nuevo más tarde.";
     }
 
     $conexion->close();
+}
+
+// Función para limpiar datos
+function limpiarDatos($datos) {
+    // Aplicar limpieza según sea necesario (puedes agregar más validaciones aquí)
+    return htmlspecialchars(trim($datos));
 }
 ?>
 
@@ -62,14 +76,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <h1>Iniciar Sesión</h1>
 
         <?php
-        if (isset($mensajeError)) {
+        if (!empty($mensajeError)) {
             echo "<p class='mensaje-error'>$mensajeError</p>";
         }
         ?>
 
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-            <label for="nombreUsuario">Nombre de Usuario:</label>
-            <input type="text" name="nombreUsuario" required><br>
+            <label for="idUsuario">ID de Usuario:</label>
+            <input type="text" name="idUsuario" required><br>
 
             <label for="contrasena">Contraseña:</label>
             <input type="password" name="contrasena" required><br>
