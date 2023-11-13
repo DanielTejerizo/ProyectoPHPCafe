504af52e6ed373ab1f83ccab52a9d0d78be0aba0
Daniel Tejerizo <daniel.tejmor@educa.jcyl.es>
2023-11-13 19:18:12 +0100

    .

---
+++ admin/controlador/ConCli.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Panel de Modificación</title>
+    <link rel="stylesheet" href="../../Css/Alta.css">
+</head>
+
+<body>
+    <h1>Panel de Modificación Clientes</h1>
+
+    <form action="../Alta-clientes.php" method="post">
+        <button type="submit">Alta</button>
+    </form>
+
+    <form action="../Baja-clientes.php" method="post">
+        <button type="submit">Baja</button>
+    </form>
+
+    <form action="Modif-clienets.php" method="post">
+        <button type="submit">Modificacion</button>
+    </form>
+
+</body>
+</html>
---
+++ admin/controlador/ConPed.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Panel de Modificación</title>
+    <link rel="stylesheet" href="../../Css/Alta.css">
+</head>
+
+<body>
+    <h1>Panel de Modificación Pedidos</h1>
+
+    <form action="../Alta-pedidos.php" method="post">
+        <button type="submit">Alta</button>
+    </form>
+
+    <form action="../Baja-pedidos.php" method="post">
+        <button type="submit">Baja</button>
+    </form>
+
+    <form action="Modif-pedidos.php" method="post">
+        <button type="submit">Modificacion</button>
+    </form>
+
+</body>
+</html>
