d7eb618c66b7e60298363c0089fc29810447e249
Daniel Tejerizo <daniel.tejmor@educa.jcyl.es>
2023-11-13 18:26:19 +0100

    .

---
+++ admin/Controlador.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Panel de Modificación</title>
+    <link rel="stylesheet" href="../Css/Alta.css">
+</head>
+
+<body>
+    <h1>Panel de Modificación</h1>
+
+    <h2>Modificar Producto</h2>
+    <a href="modificar_producto.php">Modificar Producto</a>
+
+    <h2>Modificar Proveedor</h2>
+    <a href="modificar_proveedor.php">Modificar Proveedor</a>
+
+    <!-- Agrega más enlaces para otras entidades según sea necesario -->
+
+</body>
+
+</html>
