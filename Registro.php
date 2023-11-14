3cda60c0b0883d72ea0166e3c127830c246d52a5
Daniel Tejerizo <daniel.tejmor@educa.jcyl.es>
2023-11-14 16:21:56 +0100

    .

--- Registro.php
+++ Registro.php
@@ -7,31 +7,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $idUser = htmlspecialchars($_POST["id"]);
     $contrasena = $_POST["contrasena"];
 
-    // Encriptar la contraseña
-    $hashContrasena = password_hash($contrasena, PASSWORD_DEFAULT);
-
-    $conexion = conectar();
-
-    if ($conexion->connect_error) {
-        die("Conexión fallida: " . $conexion->connect_error);
-    }
-
-    // Validaciones adicionales si es necesario
-
-    // Consulta de inserción con la contraseña encriptada
-    $consulta = $conexion->prepare("INSERT INTO Usuarios (idUsuario, Tipo, Contrasenia, idEmpleado) VALUES (?, ?, ?, ?)");
-    $tipo = "Estandar"; // O el tipo que desees asignar
-    $idEmpleado = 1; // O el idEmpleado correspondiente
-
-    $consulta->bind_param("issi", $idUser, $tipo, $hashContrasena, $idEmpleado);
-
-    if ($consulta->execute()) {
-        $mensajeExito = "Registro exitoso. ¡Bienvenido!";
+    if (strlen($contrasena) < 8) {
+        $mensajeError = "La contraseña debe tener al menos 8 caracteres.";
     } else {
-        $mensajeError = "Error al registrar el usuario: " . $conexion->error;
-    }
+        // Encriptar la contraseña
+        $hashContrasena = password_hash($contrasena, PASSWORD_DEFAULT);
 
-    $conexion->close();
+        $conexion = conectar();
+
+        if ($conexion->connect_error) {
+            die("Conexión fallida: " . $conexion->connect_error);
+        }
+
+        $consulta = $conexion->prepare("INSERT INTO Usuarios (idUsuario, Tipo, Contrasenia) VALUES (?, ?, ?)");
+        $tipo = "Estandar"; // O el tipo que desees asignar
+
+        $consulta->bind_param("iss", $idUser, $tipo, $hashContrasena);
+
+
+        try {
+            if ($consulta->execute()) {
+                $mensajeExito = "Registro exitoso. ¡Bienvenido!";
+            } else {
+                throw new Exception("Error al registrar el usuario: " . $conexion->error);
+            }
+        } catch (Exception $e) {
+            $mensajeError = $e->getMessage();
+        }
+
+        $conexion->close();
+    }
 }
 ?>
 
