67a22ac191337378330884f00307317c6a768eb2
Daniel Tejerizo <daniel.tejmor@educa.jcyl.es>
2023-11-20 18:04:12 +0100

    .

--- conexion.php
+++ conexion.php
@@ -6,10 +6,8 @@ function conectar() {
     $password = "TrabajoPHP@";
     $dbname = "trabajophp";
 
-    // Crear la conexión
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-    // Verificar la conexión
     if (!$conn) {
         die("Error de conexión: " . mysqli_connect_error());
     }
