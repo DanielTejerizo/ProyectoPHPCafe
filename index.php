c0ade8b4390745ef656c8494be53c5e469040759
DanielTejerizo <daniel.tejmor@educa.jcyl.es.com>
2023-11-20 17:12:50 +0100

    .

--- index.php
+++ index.php
@@ -15,31 +15,42 @@
     </header>
 
     <nav class="menu-container">
-    <div class="left-menu">
-        <a href="Pedido/Catálogo.php"><span>Productos</span></a>
-        <a href="Contacto.php"><span>Contacto</span></a>
-        <a href="Nosotros.php"><span>A cerca de nosotros</span></a>
-    </div>
-    <div class="right-menu">
-        <a href="Login.php"><img src="img/user-solid.png" alt="IconUser" width="20" height="20"></a>
-    </div>
-</nav>
+        <div class="left-menu">
+            <a href="Pedido/Catalogo.php"><span>Productos</span></a>
+            <a href="Contacto.php"><span>Contacto</span></a>
+            <a href="Nosotros.php"><span>A cerca de nosotros</span></a>
+        </div>
+        <div class="right-menu">
+            <a href="Login.php"><img src="img/user-solid.png" alt="IconUser" width="20" height="20"></a>
+        </div>
+    </nav>
 
 
-    <section id="seccion1">
-        <h2>Sección 1</h2>
-        <p>Contenido de la sección 1...</p>
+    <section id="inicio">
+        <h2>Bienvenido a Coolfee!</h2>
+        <p>Descubre el lugar perfecto para disfrutar del mejor café, ambiente acogedor y deliciosas opciones para
+            acompañar.</p>
     </section>
 
-    <section id="seccion2">
-        <h2>Sección 2</h2>
-        <p>Contenido de la sección 2...</p>
+
+    <section id="menu">
+        <h2>Nuestro Menú</h2>
+        <p>Sumérgete en una experiencia única con nuestra selección de cafés premium y especialidades:</p>
+        <ul>
+            <li>Café Espresso</li>
+            <li>Café Latte</li>
+            <li>Cappuccino</li>
+            <li>Americano</li>
+        </ul>
     </section>
 
-    <section id="seccion3">
-        <h2>Sección 3</h2>
-        <p>Contenido de la sección 3...</p>
+    <section id="reseñas">
+        <h2>Lo que dicen nuestros clientes</h2>
+        <p>"Coolfee es mi lugar favorito para relajarme después de un día agitado. El ambiente y el café son
+            simplemente inigualables."</p><br><br><br><br>
     </section>
+
+
 
     <footer>
         <p>&copy; 2023 Mi Página Web</p>
