50b07ac412b2da7d595afd845e5d7024a786d309
DanielTejerizo <daniel.tejmor@educa.jcyl.es.com>
2023-11-20 16:08:41 +0100

    .

--- Pedido/ProcesarPedido.php
+++ Pedido/ProcesarPedido.php
@@ -1,103 +1,85 @@
+<link rel="stylesheet" href="../css/Productos.css">
 <?php
 include '../conexion.php';
 
 session_start();
-
-function generarIdPedido() {
-    return rand(10000, 99999);
-}
-
-function obtenerIdEmpleadoAleatorio($conn) {
-    $sqlEmpleados = "SELECT idEmpleado FROM empleados";
-    $resultEmpleados = $conn->query($sqlEmpleados);
-
-    if ($resultEmpleados->num_rows > 0) {
-        $empleadosDisponibles = array();
-        while ($rowEmpleado = $resultEmpleados->fetch_assoc()) {
-            $empleadosDisponibles[] = $rowEmpleado['idEmpleado'];
-        }
-        return $empleadosDisponibles[array_rand($empleadosDisponibles)];
-    } else {
-        die("No hay empleados disponibles.");
-    }
-}
-
-function obtenerIdClientePorNombre($conn, $nombreUsuario) {
-    $sqlCliente = "SELECT idCliente FROM clientes WHERE NombreCli = ?";
-    $stmt = $conn->prepare($sqlCliente);
-    $stmt->bind_param("s", $nombreUsuario);
-    $stmt->execute();
-    $resultCliente = $stmt->get_result();
-
-    if ($resultCliente->num_rows > 0) {
-        $rowCliente = $resultCliente->fetch_assoc();
-        return $rowCliente['idCliente'];
-    } else {
-        die("No se encontró el cliente con el nombre de usuario proporcionado.");
-    }
-}
-
-function obtenerPrecioProducto($conn, $idProducto) {
-    $sqlPrecio = "SELECT Precio FROM productos WHERE idProducto = ?";
-    $stmt = $conn->prepare($sqlPrecio);
-    $stmt->bind_param("i", $idProducto);
-    $stmt->execute();
-    $resultPrecio = $stmt->get_result();
-
-    if ($resultPrecio->num_rows > 0) {
-        $rowPrecio = $resultPrecio->fetch_assoc();
-        return $rowPrecio['Precio'];
-    } else {
-        die("Error al obtener el precio del producto.");
-    }
-}
 
 // Verificar si se ha enviado el formulario desde el paso anterior
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["confirmar"])) {
     $conn = conectar();
 
     // Obtener los datos almacenados en sesiones
-    $productosSeleccionados = $_SESSION['productosSeleccionados'];
+    $idProductoSeleccionado = $_SESSION['idProducto'];
+    $cantidad = $_SESSION['cantidad'];
     $nombreUsuario = $_POST['nombre_usuario'];
 
-    // Generar un ID de pedido aleatorio
-    $idPedido = generarIdPedido();
+    // Obtener el precio del producto seleccionado
+    $sqlPrecio = "SELECT Precio FROM productos WHERE idProducto = '$idProductoSeleccionado'";
+    $resultPrecio = $conn->query($sqlPrecio);
 
-    // Obtener un ID de empleado aleatorio
-    $idEmpleado = obtenerIdEmpleadoAleatorio($conn);
-
-    // Obtener el ID del cliente a partir del nombre de usuario
-    $idCliente = obtenerIdClientePorNombre($conn, $nombreUsuario);
-
-    // Iterar sobre los productos y realizar la inserción en la tabla de pedidos
-    foreach ($productosSeleccionados as $idProducto => $cantidad) {
-        // Obtener el precio del producto seleccionado
-        $precioProducto = obtenerPrecioProducto($conn, $idProducto);
+    if ($resultPrecio->num_rows > 0) {
+        $rowPrecio = $resultPrecio->fetch_assoc();
+        $precioProducto = $rowPrecio['Precio'];
 
         // Calcular el total
         $total = $cantidad * $precioProducto;
 
-        // Realizar la inserción en la tabla de pedidos
-        $sqlInsertarPedido = "INSERT INTO pedidos (idPedido, idProducto, Cantidad, Total, idCliente, idEmpleado) VALUES (?, ?, ?, ?, ?, ?)";
-        $stmt = $conn->prepare($sqlInsertarPedido);
-        $stmt->bind_param("iiiiii", $idPedido, $idProducto, $cantidad, $total, $idCliente, $idEmpleado);
-        
-        if ($stmt->execute() !== TRUE) {
-            die("Error al registrar el pedido: " . $conn->error);
-            // Puedes manejar errores aquí, por ejemplo, redirigiendo a una página de error
-        }
-    }
+        // Generar un ID de pedido aleatorio
+        $idPedido = rand(10000, 99999);
 
-    echo "Pedido registrado correctamente. Número de Pedido: " . $idPedido;
+        // Obtener un ID de empleado aleatorio de la tabla empleados
+        $sqlEmpleados = "SELECT idEmpleado FROM empleados";
+        $resultEmpleados = $conn->query($sqlEmpleados);
+
+        if ($resultEmpleados->num_rows > 0) {
+            $empleadosDisponibles = array();
+            while ($rowEmpleado = $resultEmpleados->fetch_assoc()) {
+                $empleadosDisponibles[] = $rowEmpleado['idEmpleado'];
+            }
+
+            // Elegir aleatoriamente un ID de empleado
+            $idEmpleado = $empleadosDisponibles[array_rand($empleadosDisponibles)];
+
+            // Obtener el ID del cliente a partir del nombre de usuario
+            $sqlCliente = "SELECT idCliente FROM clientes WHERE NombreCli = '$nombreUsuario'";
+            $resultCliente = $conn->query($sqlCliente);
+
+            if ($resultCliente->num_rows > 0) {
+                $rowCliente = $resultCliente->fetch_assoc();
+                $idCliente = $rowCliente['idCliente'];
+
+                // Realizar la inserción en la tabla de pedidos
+                // (Agrega aquí tu lógica para obtener más información del empleado si es necesario)
+
+                // Ejemplo de inserción en la tabla de pedidos (ajusta según tu base de datos)
+                $sqlInsertarPedido = "INSERT INTO pedidos (idPedido, idProducto, Cantidad, Total, idCliente, idEmpleado) VALUES ('$idPedido', '$idProductoSeleccionado', '$cantidad', '$total', '$idCliente', '$idEmpleado')";
+
+                if ($conn->query($sqlInsertarPedido) === TRUE) {
+                    echo "Pedido registrado correctamente. Número de Pedido: " . $idPedido;
+                } else {
+                    echo "Error al registrar el pedido: " . $conn->error;
+                }
+            } else {
+                echo "No se encontró el cliente con el nombre de usuario proporcionado.";
+            }
+        } else {
+            echo "No hay empleados disponibles.";
+        }
+    } else {
+        echo "Error al obtener el precio del producto.";
+    }
 
     // Cerrar la conexión
     $conn->close();
 
     // Limpiar las sesiones después de completar el pedido
-    unset($_SESSION['productosSeleccionados']);
+    unset($_SESSION['idProducto']);
+    unset($_SESSION['cantidad']);
 } else {
     // Si no se ha enviado el formulario correctamente, redirigir al catálogo de productos
     header("Location: Catalogo.php");
     exit();
 }
 ?>
+<body>
+    <a href="Catalogo.php">Compra más!!</a>
