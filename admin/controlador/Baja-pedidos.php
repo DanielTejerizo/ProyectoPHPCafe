bc78fe737403af61d73811ea6cc78c8133b5742b
Daniel Tejerizo <daniel.tejmor@educa.jcyl.es>
2023-11-20 18:17:32 +0100

    .

--- admin/controlador/Baja-pedidos.php
+++ admin/controlador/Baja-pedidos.php
@@ -2,28 +2,28 @@
 include('../../conexion.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
-    // Verificar si se ha enviado el formulario y si está definido el campo idPedidoBaja
+
     if (isset($_POST["idPedidoBaja"])) {
-        // Obtener el ID del pedido a dar de baja
+
         $idPedidoBaja = intval($_POST["idPedidoBaja"]);
 
-        // Conectar a la base de datos
+
         $conexion = conectar();
 
-        // Preparar la consulta de eliminación
+
         $secuenciaBaja = $conexion->prepare("DELETE FROM Pedidos WHERE idPedido = ?");
 
-        // Corregir la línea de bind_param
+
         $secuenciaBaja->bind_param('i', $idPedidoBaja);
 
-        // Ejecutar la consulta
+
         if ($secuenciaBaja->execute()) {
             echo "Pedido dado de baja exitosamente";
         } else {
             echo "Error al dar de baja el pedido: " . $secuenciaBaja->error;
         }
 
-        // Cerrar la conexión
+
         $conexion->close();
     } else {
         echo "ID del pedido no definido.";
@@ -50,13 +50,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
 
     <div>
         <?php
-        // Conectar a la base de datos
+
         $conexion = conectar();
 
-        // Consultar la tabla de pedidos ordenados por ID
+
         $consultaPedidos = $conexion->query("SELECT idPedido, idProducto, Cantidad, Total, idCliente, idEmpleado FROM Pedidos ORDER BY idPedido");
 
-        // Verificar si hay resultados
+
         if ($consultaPedidos->num_rows > 0) {
             echo "<h2>Lista de Pedidos</h2>";
             echo "<table border='1'>
@@ -85,12 +85,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
             echo "No hay pedidos registrados.";
         }
 
-        // Cerrar la conexión
         $conexion->close();
         ?>
     </div>
 
-    <!-- Formularios para otras operaciones (Modificación, Alta, etc.) -->
+
     <form action="../Alta-pedidos.php" method="post">
         <button type="submit">Alta</button>
     </form>
