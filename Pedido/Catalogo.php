252cf006c15cc6b930fa2baf969b52f9e7d52654
DanielTejerizo <daniel.tejmor@educa.jcyl.es.com>
2023-11-22 22:32:48 +0100

    .

--- Pedido/Catalogo.php
+++ Pedido/Catalogo.php
@@ -1,22 +1,67 @@
 <?php
 include '../conexion.php';
 
+// Verificar si se ha enviado el formulario desde el paso anterior
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["comprar"])) {
-
+    // Obtener los datos del producto y cantidad
     $idProductoSeleccionado = $_POST['idProducto'];
     $cantidad = $_POST['cantidad'];
 
-
+    // Almacenar los datos en sesiones para usarlos en el siguiente paso
     session_start();
     $_SESSION['idProducto'] = $idProductoSeleccionado;
     $_SESSION['cantidad'] = $cantidad;
 
-
+    // Redirigir a la página para ingresar el nombre de usuario
     header("Location: IngresarNombreUsuario.php");
     exit();
 }
 
+// Filtrar productos por categoría y precio
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["filtrar"])) {
+    // Obtener valores de los filtros
+    $filtroCategoria = $_POST['categoria'];
+    $filtroPrecioMin = $_POST['precio_min'];
+    $filtroPrecioMax = $_POST['precio_max'];
 
+    $conexion = conectar();
+
+    if ($conexion->connect_error) {
+        die("Conexión fallida: " . $conexion->connect_error);
+    }
+
+    // Consulta para obtener productos filtrados
+    $sql = "SELECT idProducto, NombreProd, Precio FROM productos WHERE 1=1";
+
+    if (!empty($filtroCategoria)) {
+        $sql .= " AND IdCategoria = $filtroCategoria";
+    }
+
+    if (!empty($filtroPrecioMin)) {
+        $sql .= " AND Precio >= $filtroPrecioMin";
+    }
+
+    if (!empty($filtroPrecioMax)) {
+        $sql .= " AND Precio <= $filtroPrecioMax";
+    }
+
+    $result = $conexion->query($sql);
+
+    $conexion->close();
+} else {
+    // Si no se ha enviado el formulario de filtrado, mostrar todos los productos
+    $conexion = conectar();
+
+    if ($conexion->connect_error) {
+        die("Conexión fallida: " . $conexion->connect_error);
+    }
+
+    // Consulta para obtener todos los productos disponibles
+    $sql = "SELECT idProducto, NombreProd, Precio FROM productos";
+    $result = $conexion->query($sql);
+
+    $conexion->close();
+}
 ?>
 
 <!DOCTYPE html>
@@ -32,17 +77,43 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["comprar"])) {
     <div class="catalogo-container">
         <h1>Catálogo de Productos</h1>
 
-        <?php
-
+        <!-- Formulario de filtros -->
+        <form method='post' action='Catalogo.php'>
+            <label for='categoria'>Categoría:</label>
+            <select name='categoria'>
+                <option value=''>Todas las categorías</option>
+                <?php
         $conexion = conectar();
 
         if ($conexion->connect_error) {
             die("Conexión fallida: " . $conexion->connect_error);
         }
 
-        $sql = "SELECT idProducto, NombreProd, Precio FROM productos";
-        $result = $conexion->query($sql);
+        // Consulta para obtener las categorías desde la base de datos
+        $sqlCategorias = "SELECT idCategoria, NombreCat FROM categoria";
+        $resultCategorias = $conexion->query($sqlCategorias);
 
+        if ($resultCategorias->num_rows > 0) {
+            while ($rowCategoria = $resultCategorias->fetch_assoc()) {
+                echo "<option value='" . $rowCategoria['idCategoria'] . "'>" . $rowCategoria['NombreCat'] . "</option>";
+            }
+        }
+
+        $conexion->close();
+        ?>
+            </select>
+
+            <label for='precio_min'>Precio mínimo:</label>
+            <input type='number' name='precio_min' min='0' step='0.01' placeholder='0.00'>
+
+            <label for='precio_max'>Precio máximo:</label>
+            <input type='number' name='precio_max' min='0' step='0.01' placeholder='0.00'>
+
+            <button type='submit' name='filtrar'>Filtrar</button>
+        </form>
+
+        <?php
+        // Mostrar productos según los resultados de la consulta
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 echo "<div class='producto'>";
@@ -57,10 +128,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["comprar"])) {
                 echo "</div>";
             }
         } else {
-            echo "<p>No hay productos disponibles.</p>";
+            echo "<p>No hay productos disponibles según los filtros seleccionados.</p>";
         }
-
-        $conexion->close();
         ?>
     </div>
 </body>
